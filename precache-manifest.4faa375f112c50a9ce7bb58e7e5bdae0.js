84e45c2e3af6bf6e4a3ad0a3d53c4ff522609f49
latcho.nikolov@gmail.com <niceslacker77>
2019-09-12 11:53:56 +0300

    Updates

---
+++ precache-manifest.4faa375f112c50a9ce7bb58e7e5bdae0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bab777732636a796bbbaf462e64154bc",
+    "url": "/book-catalog-react/index.html"
+  },
+  {
+    "revision": "c6f57038c81a1575ff6d",
+    "url": "/book-catalog-react/static/css/main.f666b567.chunk.css"
+  },
+  {
+    "revision": "d6e845f7f8130f9a7e9e",
+    "url": "/book-catalog-react/static/js/2.33b28638.chunk.js"
+  },
+  {
+    "revision": "c6f57038c81a1575ff6d",
+    "url": "/book-catalog-react/static/js/main.64e8316b.chunk.js"
+  },
+  {
+    "revision": "eda898dcde947d4b4fe0",
+    "url": "/book-catalog-react/static/js/runtime~main.c8022e8c.js"
+  }
+]);
